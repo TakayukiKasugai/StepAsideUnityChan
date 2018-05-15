48ed098c5fb75b7c3846c747aa8b21768a737534
TakayukiKasugai <kasugai.t@jp.fujitsu.com>
2018-05-15 11:45:04 +0900

    課題：不要になったアイテムを順次破棄しましょう　初回提出です

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+	//シーンないのユニティちゃんオブジェクトを格納
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		
+		//シーン内のユニティちゃんオブジェクトを取得
+		unitychan = GameObject.Find ("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ユニティちゃんより後ろ５mにコイン、コーン、車がある場合
+		if (unitychan.transform.position.z - this.transform.position.z > 5) {
+			//オブジェクトを破棄
+			Destroy (this.gameObject);
+		}
+	}
+}
